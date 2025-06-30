d249dd46674c87c46db99ea32bda3a3cc24532bf
Girinda <girindade@gmail.com>
2025-06-30 17:01:59 +0200

    Update exception.handling.py

--- exception.handling.py
+++ exception.handling.py
@@ -5,3 +5,19 @@
       print("something went wrong")
   finally:
       print("the 'try exept' is finished")
+
+or
+
+
+print(x)
+  except: NameError
+      print("veriable x is not defined")
+finally:
+      print("everything went wrong")
+
+x = -1
+
+if x < 0:
+
+
+
