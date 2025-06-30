9a0579a0e5c19502838babbf98186cbf2890c8eb
Girinda <girindade@gmail.com>
2025-06-30 16:24:43 +0200

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,1 +1,8 @@
-1
+
+def area (length, width):
+    #function to calculate the area
+    return length * width 
+
+def perimeter(length, width):
+    #Function to calculate the perimeter 
+    return * (length + width)
