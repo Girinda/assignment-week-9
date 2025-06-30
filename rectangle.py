e109f8ffbcad847cb806bd9fc775515514839750
Girinda <girindade@gmail.com>
2025-06-30 16:33:59 +0200

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,11 @@
-1
+import calculator
+
+length = float (input("enter the length of the rectangle: "))
+width = float (input("enter the width of the rectangle:"))
+
+area = calculate.area(length,width)
+perimeter = calculate.perimeter(length, width)
+
+print (f"the area of the rectangle is:{area}")
+print (f"the perimeter of the rectangle is: {perimeter}")
+
